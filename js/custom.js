707c2e107c60b74e78b422aaa3211dd79b6ed093
Nguyen Dinh Thanh <thanhnd@bap.jp>
2020-02-19 22:59:06 +0700

    Updated!

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,7 @@
+// CUSTOM JAVASCRIPT FOR SELECT PAGE
+/*
+    Control Toggle Menu Bar
+*/
 $(document).ready(function(){
   var contador = 1;
   $('.header-content .col-md-2 span').click(function(){
@@ -5,7 +9,6 @@ $(document).ready(function(){
       $('.header-content .wraper-menu-main').animate({
         left: '0'
       });
-
       contador = 0;
     } else {
       contador = 1;
@@ -15,14 +18,5 @@ $(document).ready(function(){
     }
     $('.header-content .col-md-2 span').removeClass('hidden')
     $(this).toggleClass('hidden');
-
   });
-
-  // $('.header-top nav ul li.menu-item-has-children a.opener').click(function(e){
-  //   e.preventDefault();
-  //   $(this).siblings('ul.sub-menu').slideToggle();
-  //   // alert(1);
-  //   $(this).children('i').toggleClass('hidden');
-
-  // });
-})
+});
